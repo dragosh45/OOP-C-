9460328ca2be5b02357f9076e47cc2a4c50152d5
dragosh45 <dragos.preda20@yahoo.com>
2023-01-22 18:40:15 +0200

    initial commit

---
+++ ACM2.BLTest/CustomerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ACM2.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // -- Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "pipi";
+            customer.LastName = "popo";
+            string expected = "popo, pipi";
+            // -- Act
+            string actual = customer.FullName;
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            // -- Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "pipi";
+
+            string expected = "pipi";
+            // -- Act
+            string actual = customer.FullName;
+            // -- Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void StaticTest()
+        {
+            // -- Arrange
+            Customer customer1 = new Customer();
+            Customer.InstanceCount += 1;
+            Customer customer2 = new Customer();
+            Customer.InstanceCount += 1;
+            Customer customer3 = new Customer();
+            Customer.InstanceCount += 1;
+            // -- Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+    }
+}
---
+++ ACM2.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM2.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ACM2.BLTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("52234c48-3492-43d1-b959-7cf7390b537b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
